33f9702b66390a54b0030be36f04516f7d68ffa0
Praneeth <praneethbedapudi@gmail.com>
2020-07-17 01:46:59 +0530

    update checkpoints and classes in detector
    Signed-off-by: Praneeth <praneethbedapudi@gmail.com>

--- nudenet/detector.py
+++ nudenet/detector.py
@@ -30,34 +30,46 @@ def read_image_bgr(path):
 
     return image[:, :, ::-1]
 
+FILE_URLS = {
+    'default': {
+        'checkpoint': 'https://github.com/notAI-tech/NudeNet/releases/download/v0/detector_v2_default_checkpoint',
+        'classes': 'https://github.com/notAI-tech/NudeNet/releases/download/v0/detector_v2_default_classes'
+    },
+    'base': {
+        'checkpoint': 'https://github.com/notAI-tech/NudeNet/releases/download/v0/detector_v2_base_checkpoint',
+        'classes': 'https://github.com/notAI-tech/NudeNet/releases/download/v0/detector_v2_base_classes'
+    }
+}
+
 class Detector():
     detection_model = None
-    classes = [
-        'BELLY',
-        'BUTTOCKS',
-        'F_BREAST',
-        'F_GENITALIA',
-        'M_GENITALIA',
-        'M_BREAST',
-    ]
+    classes = None
     
-    def __init__(self):
+    def __init__(self, model_name='default'):
         '''
             model = Detector()
         '''
-        url = 'https://github.com/bedapudi6788/NudeNet/releases/download/v0/detector_model'
+        checkpoint_url = FILE_URLS['default']['checkpoint']
+        classes_url = FILE_URLS['default']['classes']
+
         home = os.path.expanduser("~")
-        model_folder = os.path.join(home, '.NudeNet/')
+        model_folder = os.path.join(home, f'.NudeNet/{model_name}')
         if not os.path.exists(model_folder):
-            os.mkdir(model_folder)
+            os.makedirs(model_folder)
         
-        model_path = os.path.join(model_folder, 'detector')
+        checkpoint_path = os.path.join(model_folder, 'checkpoint')
+        classes_path = os.path.join(model_folder, 'classes')
 
-        if not os.path.exists(model_path):
-            print('Downloading the checkpoint to', model_path)
-            pydload.dload(url, save_to_path=model_path, max_time=None)
+        if not os.path.exists(checkpoint_path):
+            print('Downloading the checkpoint to', checkpoint_path)
+            pydload.dload(checkpoint_url, save_to_path=checkpoint_path, max_time=None)
+        
+        if not os.path.exists(classes_path):
+            print('Downloading the classes list to', classes_path)
+            pydload.dload(classes_url, save_to_path=classes_path, max_time=None)
 
-        Detector.detection_model = models.load_model(model_path, backbone_name='resnet101')
+        Detector.detection_model = models.load_model(checkpoint_path, backbone_name='resnet50')
+        Detector.classes = [c.strip() for c in open(classes_path).readlines() if c.strip()]
     
     def detect_video(self, video_path, min_prob=0.6, batch_size=2):
         frame_indices, frames, fps, video_length = get_interest_frames_from_video(video_path)
@@ -111,6 +123,7 @@ class Detector():
     def censor(self, img_path, out_path=None, visualize=True, parts_to_blur=['BELLY', 'BUTTOCKS', 'F_BREAST', 'F_GENITALIA', 'M_GENETALIA', 'M_BREAST']):
         if not out_path and not visualize:
             print('No out_path passed and visualize is set to false. There is no point in running this function then.')
+            return
 
         image = cv2.imread(img_path)
         boxes = Detector.detect(self, img_path)
@@ -119,6 +132,8 @@ class Detector():
         for box in boxes:
             part = image[box[1]:box[3], box[0]:box[2]]
             image = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (0, 0, 0), cv2.FILLED)
+            # image = cv2.GaussianBlur(part,(23, 23), 30)
+            # image[box[1]:box[3], box[0]:box[2]] = part
         
         if visualize:
             cv2.imshow("Blurred image", image)
@@ -127,7 +142,6 @@ class Detector():
         if out_path:
             cv2.imwrite(out_path, image)
 
-
 if __name__ == '__main__':
     m = Detector()
-    print(m.censor('/Users/bedapudi/Desktop/n2.jpg', out_path='a.jpg'))
+    print(m.detect('/Users/bedapudi/Desktop/n2.jpg'))
